dd0f314e8bdb41c250260f992365a11c16d42b15
sahildaswani <sahildaswani18@gmail.com>
2022-03-03 12:38:04 -0500

    Made code more modular: moved majorlist to another file, created a new component for SearchBar, created a new file for theme + changed search to a functional component

--- src/App.js
+++ src/App.js
@@ -1,64 +1,10 @@
-import * as React from "react";
-import { styled, alpha } from "@mui/material/styles";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import AccountCircle from "@mui/icons-material/AccountCircle";
-import Typography from "@mui/material/Typography";
-import InputBase from "@mui/material/InputBase";
-import MenuIcon from "@mui/icons-material/Menu";
-import SearchIcon from "@mui/icons-material/Search";
-import Search from "./components/search.jsx";
-import { createTheme } from "@mui/material/styles";
-import { ThemeProvider } from "@emotion/react";
-import CssBaseline from "@mui/material/CssBaseline";
-const theme = createTheme({
-  palette: {
-    background: {
-      default: "#222222",
-    },
-    primary: {
-      main: "#456A69",
-    },
-    secondary: {
-      main: "#edf2ff",
-    },
-  },
-});
+import React from 'react'
+import SearchBar from './components/SearchBar';
 
-export default function SearchAppBar() {
+export default function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Box
-        sx={{
-          flexGrow: 1,
-        }}
-      >
-        <AppBar position="static">
-          <Toolbar>
-            <Typography
-              variant="h6"
-              noWrap
-              component="div"
-              sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
-            >
-              UF Students Social Network
-            </Typography>
-            <Search></Search>
-            <IconButton
-              size="large"
-              edge="end"
-              aria-label="account of current user"
-              aria-haspopup="true"
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    </ThemeProvider>
+    <div className="App">
+      <SearchBar />
+    </div>
   );
 }
---
+++ src/Data/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    background: {
+      default: "#222222",
+    },
+    primary: {
+      main: "#456A69",
+    },
+    secondary: {
+      main: "#edf2ff",
+    },
+  },
+});

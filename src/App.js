1dd1a0de61b1cf01653d6ab75df6348002700dc5
sahildaswani <sahildaswani18@gmail.com>
2022-03-04 03:50:27 -0500

    Created functionality of the create profile page and implimented it with the firestore database. UI still needs to be finished.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,34 @@ import React from 'react'
 import SearchBar from './components/SearchBar';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
+import CreateProfile from './components/CreateProfile';
 import { BrowserRouter as Router, Routes, Route, useNavigate} from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import { auth } from './firebase';
+import { auth, db } from './firebase';
 import { onAuthStateChanged } from 'firebase/auth';
+import { doc, getDoc, setDoc } from "firebase/firestore";
 
 export default function App() {
   const [user, setUser] = useState();
+  const [profile, setProfile] = useState();
 
   useEffect(() => {
     onAuthStateChanged(auth, user => setUser(user));
   }, [])
 
-  let navigate = useNavigate();
+  useEffect(() => {
+    if (user) {
+      fetchProfile();
+    }
+  }, [user])
 
+  useEffect(() => {
+    createProfile();
+  // eslint-disable-next-line
+  }, [profile])
+
+  //Navigaation
+  let navigate = useNavigate();
   useEffect(() => {
     if (!user){
       // change path to sign in page
@@ -26,10 +40,30 @@ export default function App() {
         navigate(`/signin`);
       }
     }
+    else if (profile === undefined){
+      navigate(`/createprofile`);
+    }
     else {
       navigate(`/`);
     }
-  }, [user]);
+  // eslint-disable-next-line
+  }, [user, profile]);
+
+  async function createProfile() {
+    await setDoc(doc(db, "Profiles", auth.currentUser.uid), profile);
+  }
+
+  async function fetchProfile() {
+    const docRef = doc(db, "Profiles", auth.currentUser.uid);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists) {
+      setProfile(docSnap.data());
+    }
+  }
+  
+  const handleCreateProfile = (profile) => {
+    setProfile(profile);
+  }
 
   return (
     <div className="App">
@@ -37,6 +71,7 @@ export default function App() {
         <Route path="/" element={<SearchBar/>} />
         <Route path="/signin" element={<SignIn/>} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/createprofile" element={<CreateProfile handleCreateProfile={handleCreateProfile}/>} />
       </Routes>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut} from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyBljqnzY3XdsM7YhlLDCHxXDndSCU1oBR0",
@@ -13,7 +14,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth();
+export const db = getFirestore();
 
 export function signup(email,passowrd){
   return createUserWithEmailAndPassword(auth, email,passowrd);

933ff81f5ac596703d3c3187143275fe6ce5fca0
WNguyen14 <wnguyen1@ufl.edu>
2022-03-08 00:55:15 -0500

    WIP adding functionality add/remove profile info

--- src/components/CreateProfile.jsx
+++ src/components/CreateProfile.jsx
@@ -1,27 +1,26 @@
+import React, { useRef, useState } from "react";
+import Stack from "@mui/material/Stack";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Box from "@mui/material/Box";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
+import FormControl from "@mui/material/FormControl";
+import AddIcon from "@mui/icons-material/Add";
+import Cards from "./Card";
 
-import React, { useRef, useState } from 'react';
-import Stack from '@mui/material/Stack';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Box from '@mui/material/Box';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
-import IconButton from '@mui/material/IconButton';
-import FormControl from '@mui/material/FormControl';
-import AddIcon from '@mui/icons-material/Add';
-import Cards from './Card';
-
-const theme = createTheme();
+import { theme } from "../Data/theme";
 
 const CreateProfile = ({ handleCreateProfile }) => {
   const majorInput = useRef(null);
@@ -44,106 +43,123 @@ const CreateProfile = ({ handleCreateProfile }) => {
 
   const validateMajor = () => {
     return majors.length > 0;
-  }
+  };
 
   const validateCourses = () => {
     return courses.length > 0;
-  }
+  };
 
   const vaildateInterests = () => {
     return interests.length > 2;
-  }
+  };
 
   const addMajor = () => {
-
-    if (majorInput.current.value !== '' && !majors.includes(majorInput.current.value)) {
+    if (
+      majorInput.current.value !== "" &&
+      !majors.includes(majorInput.current.value)
+    ) {
       setMajors([...majors, majorInput.current.value]);
-      majorInput.current.value = '';
+      majorInput.current.value = "";
     }
-  }
+  };
 
   const addCourse = () => {
-    if (coursesInput.current.value !== '' && !courses.includes(coursesInput.current.value)) {
+    if (
+      coursesInput.current.value !== "" &&
+      !courses.includes(coursesInput.current.value)
+    ) {
       setCourses([...courses, coursesInput.current.value]);
-      coursesInput.current.value = '';
+      coursesInput.current.value = "";
     }
-  }
+  };
 
   const addInterest = () => {
-    if (interestsInput.current.value !== '' && !interests.includes(interestsInput.current.value)) {
+    if (
+      interestsInput.current.value !== "" &&
+      !interests.includes(interestsInput.current.value)
+    ) {
       setInterests([...interests, interestsInput.current.value]);
-      interestsInput.current.value = '';
+      interestsInput.current.value = "";
     }
-  }
+  };
 
-  function onSubmit(){
+  function onSubmit() {
     const profile = {
       firstName: firstName,
       lastName: lastName,
       majors: majors,
       courses: courses,
       interests: interests,
-    }
-    if (validateMajor() && validateCourses() && vaildateInterests() && firstName !== '' && lastName !== '') {
+    };
+    if (
+      validateMajor() &&
+      validateCourses() &&
+      vaildateInterests() &&
+      firstName !== "" &&
+      lastName !== ""
+    ) {
       handleCreateProfile(profile);
-    }
-    else {
+    } else {
       let err = [];
-      if (firstName === '') {
-        err.push('First name is required');
+      if (firstName === "") {
+        err.push("First name is required");
       }
-      if (lastName === '') {
-        err.push('Last name is required');
+      if (lastName === "") {
+        err.push("Last name is required");
       }
       if (!validateMajor()) {
-        err.push('Please select at least one major');
+        err.push("Please select at least one major");
       }
       if (!validateCourses()) {
-        err.push('Please select at least one course');
+        err.push("Please select at least one course");
       }
       if (!vaildateInterests()) {
-        err.push('Please select at least three interests');
+        err.push("Please select at least three interests");
       }
-      alert(err.join('\n'));
+      alert(err.join("\n"));
     }
   }
 
   return (
     <>
       <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
           <Box
             sx={{
               marginTop: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
             }}
           >
-            <Avatar sx={{ p: 3, m: 1, bgcolor: 'secondary.main' }}>
-              <AccountCircleIcon fontSize={'large'}/>
+            <Avatar sx={{ p: 3, m: 1, bgcolor: "secondary.main" }}>
+              <AccountCircleIcon fontSize={"large"} />
             </Avatar>
             <Typography component="h1" variant="h4" gutterBottom>
               Create Profile
             </Typography>
           </Box>
-          <Box sx={{ 
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            mt: 3,
-          }}>
-            <Box sx={{
-              display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'center',
-            }}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              mt: 3,
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+              }}
+            >
               <TextField
                 onChange={onFirstNameChange}
                 sx={{
                   marginRight: 1,
-                  width: '100%',
+                  width: "100%",
                 }}
                 required
                 id="firstName"
@@ -156,7 +172,7 @@ const CreateProfile = ({ handleCreateProfile }) => {
                 onChange={onLastNameChange}
                 sx={{
                   marginLeft: 1,
-                  width: '100%',
+                  width: "100%",
                 }}
                 required
                 id="lasttName"
@@ -166,15 +182,16 @@ const CreateProfile = ({ handleCreateProfile }) => {
                 autoFocus
               />
             </Box>
-            <Box sx={{
-              display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'left',
-              mt: 1,
-              flexWrap: 'wrap',
-            }}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "left",
+                mt: 1,
+                flexWrap: "wrap",
+              }}
+            >
               {majors.map((major, i) => (
-                
                 <Cards
                   key={i}
                   text={major}
@@ -182,33 +199,34 @@ const CreateProfile = ({ handleCreateProfile }) => {
                 />
               ))}
             </Box>
-            <FormControl sx={{ mt: 1, width: '100%'}} variant="outlined">
-              <InputLabel htmlFor="outlined-adornment-majors">Add Major</InputLabel>
+            <FormControl sx={{ mt: 1, width: "100%" }} variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-majors">
+                Add Major
+              </InputLabel>
               <OutlinedInput
                 id="outlined-adornment-majors"
-                type={'text'}
+                type={"text"}
                 label="Add Major"
                 name="majors"
                 inputRef={majorInput}
                 endAdornment={
                   <InputAdornment position="end">
-                    <IconButton
-                      onClick={addMajor}
-                      edge="end"
-                    >
+                    <IconButton onClick={addMajor} edge="end">
                       <AddIcon />
                     </IconButton>
                   </InputAdornment>
                 }
               />
             </FormControl>
-            <Box sx={{
-              display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'left',
-              mt: 1,
-              flexWrap: 'wrap',
-            }}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "left",
+                mt: 1,
+                flexWrap: "wrap",
+              }}
+            >
               {courses.map((course, i) => (
                 <Cards
                   key={i}
@@ -217,55 +235,57 @@ const CreateProfile = ({ handleCreateProfile }) => {
                 />
               ))}
             </Box>
-            <FormControl sx={{ mt: 1, width: '100%'}} variant="outlined">
-              <InputLabel htmlFor="outlined-adornment-courses">Add Course</InputLabel>
+            <FormControl sx={{ mt: 1, width: "100%" }} variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-courses">
+                Add Course
+              </InputLabel>
               <OutlinedInput
                 id="outlined-adornment-courses"
-                type={'text'}
+                type={"text"}
                 label="Add Course"
                 name="courses"
                 inputRef={coursesInput}
                 endAdornment={
                   <InputAdornment position="end">
-                    <IconButton
-                      onClick={addCourse}
-                      edge="end"
-                    >
+                    <IconButton onClick={addCourse} edge="end">
                       <AddIcon />
                     </IconButton>
                   </InputAdornment>
                 }
               />
             </FormControl>
-            <Box sx={{
-              display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'left',
-              mt: 1,
-              flexWrap: 'wrap',
-            }}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "left",
+                mt: 1,
+                flexWrap: "wrap",
+              }}
+            >
               {interests.map((interest, i) => (
                 <Cards
                   key={i}
                   text={interest}
-                  click={() => setInterests(interests.filter((m, j) => i !== j))}
+                  click={() =>
+                    setInterests(interests.filter((m, j) => i !== j))
+                  }
                 />
               ))}
             </Box>
-            <FormControl sx={{ mt: 1, width: '100%'}} variant="outlined">
-              <InputLabel htmlFor="outlined-adornment-interests">Add Interest</InputLabel>
+            <FormControl sx={{ mt: 1, width: "100%" }} variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-interests">
+                Add Interest
+              </InputLabel>
               <OutlinedInput
                 id="outlined-adornment-interests"
-                type={'text'}
+                type={"text"}
                 label="Add Interest"
                 name="interests"
-                inputRef={interestsInput} 
+                inputRef={interestsInput}
                 endAdornment={
                   <InputAdornment position="end">
-                    <IconButton
-                      onClick={addInterest}
-                      edge="end"
-                    >
+                    <IconButton onClick={addInterest} edge="end">
                       <AddIcon />
                     </IconButton>
                   </InputAdornment>
@@ -285,9 +305,9 @@ const CreateProfile = ({ handleCreateProfile }) => {
               Create Profile
             </Button>
           </Box>
-      </Container>
+        </Container>
       </ThemeProvider>
     </>
   );
-}
-export default CreateProfile
+};
+export default CreateProfile;
--- src/components/Selections.jsx
+++ src/components/Selections.jsx
@@ -1,10 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { db, auth } from "../firebase.js";
 import { doc, getDoc } from "firebase/firestore";
 import firebase from "firebase/compat/app";
 import { getAuth, signOut } from "firebase/auth";
 import { Firestore } from "firebase/firestore";
 import Box from "@mui/material/Box";
+import { Divider, TextField } from "@mui/material";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
+import FormControl from "@mui/material/FormControl";
+import AddIcon from "@mui/icons-material/Add";
 const Selections = () => {
   const auth = getAuth();
   const user = auth.currentUser;
@@ -18,9 +25,30 @@ const Selections = () => {
     setUserInterests(data.interests);
   }
   const [userName, setUserName] = useState("");
-  const [userMajor, setUserMajor] = useState("");
-  const [userInterests, setUserInterests] = useState("");
+  const [userMajor, setUserMajor] = useState([]);
+  const [userInterests, setUserInterests] = useState([]);
 
+  const majorInput = useRef(null);
+  const coursesInput = useRef(null);
+  const interestsInput = useRef(null);
+
+  const addInterest = () => {
+    if (
+      interestsInput.current.value !== "" &&
+      !userInterests.includes(interestsInput.current.value)
+    ) {
+      setUserInterests([...userInterests, interestsInput.current.value]);
+      interestsInput.current.value = "";
+    }
+  };
+
+  const handleInterestSubmit = () => {
+    addInterest();
+    user.updateProfile({
+      firstName: "newName",
+      interests: userInterests,
+    });
+  };
   useEffect(() => {
     fetchProfile();
   }, []);
@@ -36,10 +64,32 @@ const Selections = () => {
         height: 750,
       }}
     >
-      <h1>Current User</h1>
-      <h2>{userName}</h2>
-      <h3>{userMajor}</h3>
-      <h4>{userInterests}</h4>
+      <h1>Profile</h1>
+      <Divider />
+      <h3>{userName}</h3>
+      <Divider />
+      <h4>Major</h4>
+      <FormControl sx={{ mt: 1, width: "100%" }} variant="outlined">
+        <InputLabel htmlFor="outlined-adornment-majors">Add Major</InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-majors"
+          type={"text"}
+          label="Add Major"
+          name="majors"
+          inputRef={interestsInput}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton onClick={handleInterestSubmit} edge="end">
+                <AddIcon />
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+      <h6>{userMajor}</h6>
+      <Divider />
+      <h4>Interests</h4>
+      <h6>{userInterests}</h6>
     </Box>
   );
 };

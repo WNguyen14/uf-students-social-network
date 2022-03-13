41ec54856edbf4ff53e40c8913bcd713a169440e
WNguyen14 <wnguyen1@ufl.edu>
2022-03-13 18:12:15 -0400

    Updates

---
+++ precache-manifest.e3a689bdc153e5fbe71908a4b4b08987.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3597487fcb73738f2399",
+    "url": "/uf-students-social-network/static/css/main.d265c8c1.chunk.css"
+  },
+  {
+    "revision": "3597487fcb73738f2399",
+    "url": "/uf-students-social-network/static/js/main.3597487f.chunk.js"
+  },
+  {
+    "revision": "9875db2efa6e6d5c0233",
+    "url": "/uf-students-social-network/static/js/1.9875db2e.chunk.js"
+  },
+  {
+    "revision": "6b01427d1770bb9018c3",
+    "url": "/uf-students-social-network/static/js/2.6b01427d.chunk.js"
+  },
+  {
+    "revision": "5ec85924bef8b18bd766",
+    "url": "/uf-students-social-network/static/js/runtime~main.5ec85924.js"
+  },
+  {
+    "revision": "e5b94a6c5a5167ec8c4364ca4ba79c44",
+    "url": "/uf-students-social-network/index.html"
+  }
+];

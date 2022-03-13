c7af87fded2986509a3554898ede0591aa88f496
WNguyen14 <wnguyen1@ufl.edu>
2022-03-13 17:52:45 -0400

    WIP adding functionality add/remove profile info

--- src/components/Selections.jsx
+++ src/components/Selections.jsx
@@ -23,32 +23,75 @@ const Selections = () => {
     setUserName(name);
     setUserMajor(data.majors);
     setUserInterests(data.interests);
+    setUserCourses(data.courses);
   }
   const [userName, setUserName] = useState("");
   const [userMajor, setUserMajor] = useState([]);
+  const [userCourses, setUserCourses] = useState([]);
+
   const [userInterests, setUserInterests] = useState([]);
 
   const majorInput = useRef(null);
   const coursesInput = useRef(null);
   const interestsInput = useRef(null);
 
-  const addInterest = () => {
-    if (
-      interestsInput.current.value !== "" &&
-      !userInterests.includes(interestsInput.current.value)
-    ) {
-      setUserInterests([...userInterests, interestsInput.current.value]);
-      interestsInput.current.value = "";
+  const addInfo = (__type) => {
+    if (__type === "interests") {
+      if (
+        interestsInput.current.value !== "" &&
+        !userInterests.includes(interestsInput.current.value)
+      ) {
+        let spacer = " ";
+        let newString = spacer.concat(interestsInput.current.value);
+        setUserInterests([...userInterests, newString]);
+        interestsInput.current.value = "";
+      }
+    } else if (__type === "courses") {
+      if (
+        coursesInput.current.value !== "" &&
+        !userCourses.includes(coursesInput.current.value)
+      ) {
+        let spacer = " ";
+        let newString = spacer.concat(coursesInput.current.value);
+        setUserCourses([...userCourses, newString]);
+        coursesInput.current.value = "";
+      }
+    } else if (__type === "majors") {
+      if (
+        majorInput.current.value !== "" &&
+        !userMajor.includes(majorInput.current.value)
+      ) {
+        let spacer = " ";
+        let newString = spacer.concat(majorInput.current.value);
+        setUserMajor([...userMajor, newString]);
+        majorInput.current.value = "";
+      }
     }
   };
-
-  const handleInterestSubmit = () => {
-    addInterest();
+  const handleMajorSubmit = () => {
+    addInfo("majors");
     user.updateProfile({
       firstName: "newName",
       interests: userInterests,
     });
   };
+
+  const handleCoursesSubmit = () => {
+    addInfo("courses");
+    user.updateProfile({
+      firstName: "newName",
+      interests: userInterests,
+    });
+  };
+
+  const handleInterestSubmit = () => {
+    addInfo("interests");
+    user.updateProfile({
+      firstName: "newName",
+      interests: userInterests,
+    });
+  };
+
   useEffect(() => {
     fetchProfile();
   }, []);
@@ -61,7 +104,7 @@ const Selections = () => {
         borderColor: "#FFFFFF",
         p: 4,
         borderRadius: 2,
-        height: 750,
+        height: 850,
       }}
     >
       <h1>Profile</h1>
@@ -70,12 +113,54 @@ const Selections = () => {
       <Divider />
       <h4>Major</h4>
       <FormControl sx={{ mt: 1, width: "100%" }} variant="outlined">
-        <InputLabel htmlFor="outlined-adornment-majors">Add Major</InputLabel>
+        <InputLabel htmlFor="outlined-adornment-majors">Add Courses</InputLabel>
         <OutlinedInput
           id="outlined-adornment-majors"
           type={"text"}
           label="Add Major"
           name="majors"
+          inputRef={majorInput}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton onClick={handleMajorSubmit} edge="end">
+                <AddIcon />
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+      <h6>{userMajor}</h6>
+      <Divider />
+      <h4>Courses</h4>
+      <FormControl sx={{ mt: 1, width: "100%" }} variant="outlined">
+        <InputLabel htmlFor="outlined-adornment-majors">Add Courses</InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-majors"
+          type={"text"}
+          label="Add Courses"
+          name="courses"
+          inputRef={coursesInput}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton onClick={handleCoursesSubmit} edge="end">
+                <AddIcon />
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+      <h6>{userCourses}</h6>
+      <Divider />
+      <h4>Interests</h4>
+      <FormControl sx={{ mt: 1, width: "100%" }} variant="outlined">
+        <InputLabel htmlFor="outlined-adornment-majors">
+          Add Interests
+        </InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-majors"
+          type={"text"}
+          label="Add Interest"
+          name="interests"
           inputRef={interestsInput}
           endAdornment={
             <InputAdornment position="end">
@@ -86,9 +171,6 @@ const Selections = () => {
           }
         />
       </FormControl>
-      <h6>{userMajor}</h6>
-      <Divider />
-      <h4>Interests</h4>
       <h6>{userInterests}</h6>
     </Box>
   );
